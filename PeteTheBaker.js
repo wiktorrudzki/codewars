e9e36e9a16e15959238382d8b02ddecc14e94607
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-08-14 16:53:51 +0200

    PeteTheBaker final version

--- PeteTheBaker.js
+++ PeteTheBaker.js
@@ -13,6 +13,12 @@ function cakes(recipe, available) {
     return minimum;
 }
 
+function cakesFinalVersion(recipe, available) {
+    return Object.keys(recipe).reduce((acc, item) => {
+        return Math.min(Math.floor(available[item]/recipe[item]), acc)
+    }, Infinity)
+}
+
 let recipe = {flour: 500, sugar: 200, eggs: 1};
 let available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
-console.log(cakes(recipe, available));
+console.log(cakesFinalVersion(recipe, available));
