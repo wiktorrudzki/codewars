775802cc1f4223b0cb3d44b73a741b997fe7c66d
Wiktor Rudzki <wiktor1rudzki@gmail.com>
2022-08-14 17:25:40 +0200

    WhichAreIn final version

--- WhichAreIn.js
+++ WhichAreIn.js
@@ -13,8 +13,12 @@ function inArray(array1,array2){
     return result.sort();
 }
 
+function inArrayFinalVersion(a1, a2) {
+    return a1.filter(i1 => a2.find(i2 => i2.includes(i1))).sort();
+}
+
 a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 
 a1 = ["live", "strong", "arp"]
 
-console.log(inArray(a1, a2))
+console.log(inArrayFinalVersion(a1, a2))
